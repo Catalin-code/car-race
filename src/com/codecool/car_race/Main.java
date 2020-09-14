c09440d929c6119b4c41baabdff1651bf64a47c2
Catalin-code <catalin.despa.code@gmail.com>
2020-09-14 21:43:01 +0300

    Completes all tasks via Step by Step guide.

--- src/com/codecool/car_race/Main.java
+++ src/com/codecool/car_race/Main.java
@@ -1,13 +1,26 @@
 package com.codecool.car_race;
 
 
+import com.codecool.car_race.vehicles.Vehicle;
+import com.codecool.car_race.vehicles.Car;
+import com.codecool.car_race.vehicles.Motorcycle;
+import com.codecool.car_race.vehicles.Truck;
+
+
 public class Main {
 
     /**
      * Creates all the vehicles that will be part of this race.
      */
     private static void createVehicles(Race race) {
+
+        for (int i = 0; i < 10; i++) {
+            race.registerRacer(new Car());
+            race.registerRacer(new Motorcycle());
+            race.registerRacer(new Truck());
+        }
     }
+
 
     /**
      * Entry point of our program. Creates one race instance and
